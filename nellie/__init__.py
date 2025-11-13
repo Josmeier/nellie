84344d715bcd7eca3eebc27de7fb4cfe08b224f4
Josefin_Sedlmeier <j.sedlmeier@campus.lmu.de>
2025-11-13 14:25:40 +0100

    testing gpu support mac

--- nellie/__init__.py
+++ nellie/__init__.py
@@ -5,27 +5,27 @@ device_type = 'cpu'
 # if it's a mac
 if platform.system() == 'Darwin':
     # if it has pytorch
-    # try:
-    #     import torch
-    #     # For Mac GPUs with MPS support
-    #     if torch.backends.mps.is_available():
-    #         import nellie.utils.torch_xp as xp
-    #         device_type = 'mps'
-    #         logger.warning('GPU packages detected, running via GPU.')
-    #     else:
-    #         import numpy as xp
-    #         device_type = 'cpu'
-    #         logger.warning('GPU packages not detected, running via CPU.')
-    # except ModuleNotFoundError:
-    import numpy as xp
-    device_type = 'cpu'
-    logger.warning('GPU packages not detected, running via CPU.')
+    try:
+        import torch
+        # For Mac GPUs with MPS support
+        if torch.backends.mps.is_available():
+            import nellie.utils.torch_xp as xp
+            device_type = 'mps'
+            logger.warning('GPU packages detected, running via GPU.')
+        else:
+            import numpy as xp
+            device_type = 'cpu'
+            logger.warning('GPU packages not detected, running via CPU.')
+    except ModuleNotFoundError:
+        import numpy as xp
+        device_type = 'cpu'
+        logger.warning('GPU packages not detected, running via CPU.')
 
-    xp_bk = None
-    import scipy.ndimage as ndi
-    from skimage import filters, morphology, measure
-
-    is_gpu = False
+        xp_bk = None
+        import scipy.ndimage as ndi
+        from skimage import filters, morphology, measure
+        
+        is_gpu = False
 
 
 # if it's an NVIDIA GPU
